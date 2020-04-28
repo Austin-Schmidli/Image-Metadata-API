02d0241d35631a5c4523ae76e4abbe6f23a63bb3
Austin Schmidli <austin.schmidli@optum.com>
2020-04-27 18:49:51 -0700

    more testing config

--- tests/test_getImageMetadata.py
+++ tests/test_getImageMetadata.py
@@ -1,5 +1,11 @@
 import pytest
 
+from getImageMetadata import getImageMetadata
 
+# Dummy tests for now!
 def test_test():
+    assert True
+
+def test_bytes():
+    getImageMetadata.bytes_to_image(None)
     assert True
