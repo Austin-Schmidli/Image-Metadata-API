7131a7669dfab620a50288c41602fb6f7ee6fcb1
Austin Schmidli <austin.schmidli@optum.com>
2020-05-03 14:05:36 -0700

    fix syntax error

--- deploy/deploy_function.py
+++ deploy/deploy_function.py
@@ -1,15 +1,10 @@
+def deploy_function(lambda_client, name: str, zip_path: str, layer_arns: list = None):
 
-def deploy_function(
-    lambda_client, name: str, zip_path: str, layer_arns: list(str) = None
-):
-
-    with open(zip_path, 'rb') as f:
+    with open(zip_path, "rb") as f:
         code_zip = f.read()
 
     print(f"\nUpdating function code for {name}")
-    response = lambda_client.update_function_code(
-        FunctionName=name, ZipFile=code_zip
-    )
+    response = lambda_client.update_function_code(FunctionName=name, ZipFile=code_zip)
     print(response)
 
     print(f"\nUpdating function configuration for {name}")
